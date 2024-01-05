65d9bde8b3fecd3d9f72723d99ff6827af204614
Alex Goode <alexgoodestudio@gmail.com>
2024-01-05 12:19:56 -0500

    added subscribe/ calendly

--- migrations/20231228191351_createEstimateRequestTable.js
+++ migrations/20231228191351_createEstimateRequestTable.js
@@ -1,11 +1,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable("estimate", (table) => {
     table.increments("estimate_id").primary();
-    table.string("first_name");
-    table.string("last_name");
+    table.string("name");
     table.string("email_address");
-    table.string("phone_number");
-    table.string("message");
     table.timestamps(true, true);
     table.string("service_type").nullable(); 
   });
