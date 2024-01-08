4d773417533a161332e95b6937e93fae0bf37715
Alex Goode <alexgoodestudio@gmail.com>
2024-01-08 14:19:06 -0500

    take3

--- knexfile.js
+++ knexfile.js
@@ -19,10 +19,7 @@ module.exports = {
     client: "postgresql",
     connection: {
       connectionString: DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: true, // Enable certificate validation
-        ca: process.env.DATABASE_SSL_CA // Include the CA certificate
-      },
+      ssl: { rejectUnauthorized: false } 
     },
     migrations: {
       directory: path.join(__dirname, 'migrations')
