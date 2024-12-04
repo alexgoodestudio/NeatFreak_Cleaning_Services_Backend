1c6ce35f978004eabdf7ba1b596b31e436d941aa
Alex Goode <alexgoodestudio@gmail.com>
2024-12-04 13:04:05 -0500

    updated responses JSON

--- seeds/responses.js
+++ seeds/responses.js
@@ -4,22 +4,22 @@ exports.seed = async function(knex) {
   const responses = [
     {
       service_type: 'pricing',
-      responses: "We don't have a one-size-fits-all pricing model because every home has unique needs. To ensure we provide a tailored service, we offer FREE  estimates. Book yours today through the 'Booking/ Estimates' tab or reach out to us at support@getneatfreakclean.com with your details.",
-      keywords: 'pricing,cost,charge,price,prices,estimate'
+      responses: "We don't have a one-size-fits-all pricing model because every home has unique needs. To ensure we provide a tailored service, we offer FREE  estimates. Book yours today through the 'Contact' tab or reach out to us at support@getneatfreakclean.com with your details.",
+      keywords: 'pricing,cost,charge,price,prices,estimate,range,money'
     },
     {
       service_type: 'move',
-      responses: "Transitioning to a new place? We've got you covered with our move-in/out cleaning services, ensuring your space is pristine. Let us help make your move smoother. Head on over to the Booking/ Estimates tab at the top to get in contact to schedule or request an estimate.",
-      keywords: 'move,moving,relocation,transition'
+      responses: "Transitioning to a new place? We've got you covered with our move-in/out cleaning services, ensuring your space is pristine. Let us help make your move smoother. Head on over to the Contact tab at the top to get in contact to schedule or request an estimate.",
+      keywords: 'move,moving,relocation,relocating,transition'
     },
     {
       service_type: 'cancellation',
-      responses: "Life happens, no problem! For cancellations, please give us a 48-hour notice and email us at support@getneatfreakclean.com. If you need to cancel within 48 hours, you can text or call us directly at (804) 661-2740, and we'll assist you.",
-      keywords: 'cancellation,cancel,canceling,notice'
+      responses: "Life happens, no problem! For cancellations or rescheduling, please give us a 48-hour notice and email us at support@getneatfreakclean.com. If you need to cancel within 48 hours, you can text or call us directly at (804) 482-0516, and we'll assist you.",
+      keywords: 'cancellation,cancel,canceling,notice,reschedule,rescheduling,unable'
     },
     {
       service_type: 'contact',
-      responses: "Got questions? We're here to help! Call us at (804) 661-2740 or email support@getneatfreakclean.com for assistance.",
+      responses: "Got questions? We're here to help! Call us at (804) 482-0516 or email support@getneatfreakclean.com for assistance.",
       keywords: 'contact,touch,questions,support,email,phone'
     },
     {
@@ -30,12 +30,12 @@ exports.seed = async function(knex) {
     {
       service_type: 'forMoreInfo',
       responses: "Need detailed support? Email us at support@getneatfreakclean.com, and we'll ensure you get the information you need.",
-      keywords: 'more info,information,details,support,additional'
+      keywords: 'more info,information,details,support,additional,speak,representative,real'
     },
     {
       service_type: 'howToBook',
-      responses: "Would you like to book an appointment? Simply click the 'Booking/Estimate' tab to schedule an estimate/ set up an appointment. Additionally you can email us your details at support@getneatfreakclean.com, and we'll follow up ASAP about setting up an apointment.",
-      keywords: 'book,hire,booking,schedule,times,appointment,estimate'
+      responses: "Would you like to book an appointment? Simply click the Contact' tab to schedule an estimate/ set up an appointment. Additionally you can email us your details at support@getneatfreakclean.com, and we'll follow up ASAP about setting up an apointment.",
+      keywords: 'book,hire,availability,booking,schedule,scheduling,make,times,appointment,estimate'
     },
     {
       service_type: 'newsletter',
@@ -44,8 +44,8 @@ exports.seed = async function(knex) {
     },
     {
       service_type: 'where',
-      responses: "We proudly serve Richmond, Virginia, and surrounding areas, bringing top-tier, eco-friendly cleaning services to your doorstep.",
-      keywords: 'where,location,Richmond,Virginia,Henrico,Short Pump,Manchester,Carytown,North Side, South Side,East End,Goochland'
+      responses: "We proudly serve Richmond, Virginia, and surrounding areas.",
+      keywords: 'where,location,Richmond,Virginia,Henrico,Short Pump,Manchester,Carytown,North Side,Varina,Hanover,Shockoe Bottom,Henrico,South Side,East End,Goochland'
     },
     {
       service_type: 'ecoFriendly',
@@ -54,7 +54,7 @@ exports.seed = async function(knex) {
     },
     {
       service_type: 'oneTimeClean',
-      responses: "Need a thorough clean for a special occasion or just a seasonal refresh? Our One-Time Clean service has got you covered, using eco-friendly products to make every corner of your home sparkle.",
+      responses: "Need a thorough clean for a special occasion or just a seasonal refresh? Our One-Time Clean service has got you covered.",
       keywords: 'one-time clean,one time,1 time,special occasion,party,event'
     },
     {
@@ -64,8 +64,8 @@ exports.seed = async function(knex) {
     },
     {
       "service_type": "ASAP",
-      "responses": "Need help right away? Email us at support@getneatfreakclean.com for assistance.",
-      "keywords": "asap,as soon,soon,times,sooner,free,night,tomorrow,today,late,early,this monday,this tuesday,this wednesday,this thursday,this friday,this saturday,this sunday"
+      "responses": "Need help right away? Call us at (804) 482-0516 for assistance.",
+      "keywords": "asap,as soon,immediate,immediately,soon,times,sooner,free,night,tomorrow,today,late,early,this monday,this tuesday,this wednesday,this thursday,this friday,this saturday,this sunday"
     },
   {
     service_type: 'services',
@@ -80,12 +80,12 @@ exports.seed = async function(knex) {
   {
     "service_type": "checkStatement",
     "responses": "Perfect! Feel free to reach out for any additional help at support@getneatfreakclean.com",
-    "keywords": "Thank you,Appreciate it,Thanks,thank,cool,awesome,great,perfect,wonderful,fantastic,that's all,all set,good to go,no more questions,no I'm good,all good,no further, that's it,fine,agreed,nothing else,all good"
+    "keywords": "sounds good,perf,Thank you,Appreciate it,Thanks,thank,cool,awesome,great,perfect,wonderful,fantastic,that's all,all set,good to go,no more questions,no I'm good,all good,no further, that's it,fine,agreed,nothing else,all good"
   },
   {
     service_type: 'discount',
-    responses: "Unfortunately we have no discounts right now, but join our news letter by follwing the subscribe tab at the top for future promotions",
-    keywords: 'discount,coupons,sale,promotions,free,deals'
+    responses: "Unfortunately we have no discounts right now, but join our news letter by follwing the subscribe tab at the top for future promotions.",
+    keywords: 'discount,discounts,veteran,veterans,teacher,sales,promotion,military,coupons,sale,promotions,free,deals,deal'
   }
   ];
 
